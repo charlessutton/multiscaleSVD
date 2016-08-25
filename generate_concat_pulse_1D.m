0031653fc77e85cf127f1db5e5d1c7b440249d86
Sutton Charles <charles.sutton@polytechnique.edu>
2016-08-25 17:30:22 +0300

    concat update

--- generate_concat_pulse_1D.m
+++ generate_concat_pulse_1D.m
@@ -1,25 +1,25 @@
-function [ data ] = generate_concat_pulse_1D( n, D, pulse_sigma, noise_sigma )
-%This function generates a dataset compossed of n 1D pulses, each sampled
-%on D points in the range [0,1] and with random mean
+function [ data ] = generate_concat_pulse_1D(k, n, D, pulse_sigma, noise_sigma )
+%   k - number of concatenated pulses
 %   n - number of pulses to generate
 %   D - num of sample points in the range [0,1]
-%   pulse_sigme - the width of the pulse
+%   pulse_sigma - the var of the pulse
 %   noise_sigma - var of the noise
 
-data = zeros(n,2*D);
-I1 = linspace(0,0.5,D);
-I2 = linspace(0.5,1,D);
+data = zeros(n,D);
+I = linspace(0,k,D);
+interval_idx = linspace(0,k,k+1);
+gap = 2*pulse_sigma ;
 
-for i=1:n
-    %random mean values in the range [0,1] for the curve balls genetated
-    mu1 = 0.5*rand;
-    mu2 = 0.5*rand + 0.5;
-    data(i,1:D) = normpdf(I1, mu1, pulse_sigma);
-    data(i,D+1:2*D) = normpdf(I2, mu2, pulse_sigma);
-
-end
+for j = 1:n
+    for i = 1:k 
+        left_idx = interval_idx(i) + gap ;
+        right_idx = interval_idx(i+1) - gap ;
+        mu = left_idx + (right_idx - left_idx )*rand;    
+        data(j,:) = data(j,:) + normpdf(I, mu, pulse_sigma);
+    end
+end 
 
 if noise_sigma > 0
-    data = data + noise_sigma*abs(randn(n, 2*D));
+    data = data + noise_sigma*abs(randn(n, D));
 end
 end
