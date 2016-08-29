ba78b9c40881f65def8ef3978b0d304fb7a4d229
Sutton Charles <charles.sutton@polytechnique.edu>
2016-08-29 12:29:42 +0300

    comments added

--- estimate_dim.m
+++ estimate_dim.m
@@ -5,8 +5,12 @@ function [ k ] = estimate_dim(Eeigenval)
 % inputs : Eeigneval matrix  ( SV * radius )
 % ouputs : estimation 
 
+    %we compute gaps 
     diff_matrix = diff(Eeigenval,1);
     avg =  mean(abs(diff_matrix),2);
+    
+    % we detect the first "gap peak" indicating the intrinsic dimension
+    % estimation
     diff_diff = diff(avg);
     k = find(diff_diff < 0, 1);
     
