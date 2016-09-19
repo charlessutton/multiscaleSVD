f7ce30b11c0beb027c414cadd17186fc0ec0853c
Sutton Charles <charles.sutton@polytechnique.edu>
2016-09-19 17:37:56 +0300

    delete unmoved files

--- distance_matrix.m
+++
@@ -1,7 +0,0 @@
-%function that returns the square matrices of norm2 distances between the
-%points of a dataset 
-% Values for all (i,j)
-function [dm] = distance_matrix(dataset)
-    y = pdist(dataset); %pairwise distance in a efficient format
-    dm = squareform(y);
-end
