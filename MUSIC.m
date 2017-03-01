f8c983fd204f7eb01d5e07a047fac33c067db1f5
Sutton Charles <sutton@csf.technion.ac.il>
2017-03-01 14:23:51 +0200

    music criterium fixed

--- MUSIC.m
+++ MUSIC.m
@@ -1,14 +1,14 @@
 %% Parameters data
 clear all
 data_options = struct();
-data_options.type = 'triangle';
-data_options.noise_level = 0.1;
-data_options.k = 4;
-data_options.n = 1000;
-data_options.D = 100;
+data_options.type = 'gaussian';
+data_options.noise_level = 0;
+data_options.k = 2;
+data_options.n = 10000;
+data_options.D = 200;
 data_options.gain = 'off';
-data_options.circular = 'on';
-data_options.width = 0.1;
+data_options.circular = 'off';
+data_options.width = 0.05;
 %% Generate data
 data = generate_data(data_options);
 %% MUSIC built in
@@ -17,22 +17,23 @@ pmusic(data, data_options.k);
 
 sv = svd(data);
 M = length(sv);
+
 likelihood = zeros(1,M);
 AIC = zeros(1,M);
 MDL = zeros(1,M);
 
-for p = fliplr(1:M)
+for p = 1:M
     likelihood(p) = LR(sv,p,data_options.n);
     AIC(p) = log(likelihood(p)) + 0.5*(M-p)*(M+p+1);
     MDL(p) = log(likelihood(p)) +  0.25*(M-p)*(M+p+1)*log(data_options.n);
 end
 
 figure;
-plot(likelihood(1:10))
+plot(likelihood)
 title('LR')
 figure;
-plot(AIC(1:10))
+plot(AIC)
 title('AIC')
 figure;
-plot(MDL(1:10))
+plot(MDL)
 title('MDL')
